ab6f22bb96c77c3da44fbf52e827b226f4b925ea
Andrew Zhidenko <andrei.zhidenko@kandasoft.com>
2025-08-07 21:57:44 +0300

    Small improvements.

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -4,9 +4,9 @@
     {
         public int ProductId { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public decimal Price { get; set; }
 
--- Migrations/20250807140705_AddGetTopNProductsProcedure.cs
+++ Migrations/20250807140705_AddGetTopNProductsProcedure.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
