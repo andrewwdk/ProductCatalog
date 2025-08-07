621a533397495272df7a89f5c6bfb150aa3f3bed
Andrew Zhidenko <andrei.zhidenko@kandasoft.com>
2025-08-07 17:13:59 +0300

    Add GetTopNProducts stored procedure.

--- DbContexts/ProductDbContext.cs
+++ DbContexts/ProductDbContext.cs
@@ -5,7 +5,7 @@ namespace ProductCatalog.DbContexts
 {
     public class ProductDbContext : DbContext
     {
-        public ProductDbContext(DbContextOptions<ProductDbContext> options): base(options) { }
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
 
---
+++ Migrations/20250807140705_AddGetTopNProductsProcedure.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductCatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetTopNProductsProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+               CREATE PROCEDURE GetTopNProducts
+                    @Count INT
+                AS
+                BEGIN
+                    SELECT TOP (@Count) *
+                    FROM Products
+                    ORDER BY Price DESC;
+                END;
+           ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS GetTopNProducts;");
+        }
+    }
+}
