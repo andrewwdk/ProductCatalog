6e77bf0751d6fce0a457cc1e3efd50a1976a4fd6
Andrew Zhidenko <andrei.zhidenko@kandasoft.com>
2025-08-08 12:07:31 +0300

    Add validation.

--- Models/Requests/AddProductRequest.cs
+++ Models/Requests/AddProductRequest.cs
@@ -1,9 +1,16 @@
-﻿namespace ProductCatalog.Models.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductCatalog.Models.Requests
 {
     public record AddProductRequest
     (
+        [Required(ErrorMessage = "Product name is required")]
+        [MaxLength(100, ErrorMessage = "Product name should be up to 100 characters")]
+        [MinLength(2, ErrorMessage = "Product name should not be less than 2 characters")]
         string Name,
+        [MaxLength(500, ErrorMessage = "Product description should be up to 500 characters")]
         string? Description,
+        [Range(0.01, double.MaxValue, ErrorMessage = "Product price should be a possitive value")]
         decimal Price
     );
 }
